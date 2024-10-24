1cc23f9305f92631c0e758c0a500600edfa2a743
TranDucVuong2003 <tranvuong310103@gmail.com>
2024-10-24 17:20:02 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const app = {
+    songs:[] ,
+
+    render: function () {
+    const htmls =  this.songs        
+    },
+    start: function () {
+        this.render();
+    }
+
+}
+
+app.start();
